ef6a2415de8a2702f14377274f02a0e6692369b9
Luis <luisjuniorfrota@gmail.com>
2022-06-27 00:26:54 -0300

    Criando salas

--- src/index.js
+++ src/index.js
@@ -5,26 +5,64 @@ const app = express()
 const server = require('http').createServer(app)
 const io = require('socket.io')(server)
 
+const bodyParser = require("body-parser")
+
+const crypto = require('crypto')
+
+const PORT = process.env.PORT || 3000
+
 app.use(express.static(path.join(__dirname, 'view')))
 app.set('views', path.join(__dirname, 'view'))
 app.engine('html', require('ejs').renderFile)
-app.set('view engine', 'html')
+app.set('view engine', 'ejs')
 
-app.use('/', (req,res) => {
-    res.render('index.html')
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+
+app.get('/chat', (req,res) => {
+    res.render('chat.ejs')
 })
 
-let messages = []
+let rooms = []
+let room
+
+app.get('/room/:id', (req,res) => {
+    res.render("chat.ejs", {Room: req.params.id})
+})
+
+app.get('/rooms', (req,res) => {
+    res.render("index.ejs", {Rooms: rooms})
+})
+
+app.post('/createroom', (req,res) => {
+    let roomid = crypto.randomBytes(16).toString("hex")
+    rooms.push({id:roomid, numClients: 0})
+    room = roomid
+
+    res.redirect(`/room/${roomid}`)
+})
+
+app.get('/', (req,res) => {
+    res.render('index.ejs', {Rooms: rooms})
+})
 
 io.on('connection', socket => {
     console.log(`Socket conectado: ${socket.id}`)
 
-    socket.emit('previousMessages', messages)
+    socket.join(room)
+
+    
+    
 
     socket.on('sendMessage', data => {
-        messages.push(data)
-        socket.broadcast.emit('receiveMessage', data)
+        socket.to(data.room).emit('receiveMessage', data)
+    })
+
+    socket.on('disconnect', (reason) => {
+         console.log(`Cliente ${socket} desconectou`)
     })
 })
 
-server.listen(3000)
+server.listen(PORT , () => {
+    console.log(`Server aberto na porta ${PORT}`)
+})
